ac081e7a7b46b405efc60bace32d0f4fba7aa3f2
arildlil <arildlil@student.matnat.uio.no>
2016-02-10 19:16:04 +0100

    Started working on Game Info help.

---
+++ content/dota_addons/forest_rts/panorama/scripts/custom_game/worker_count.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// Sets the worker count field.
+function SetWorkerCount(keys) {
+    var maxWorkerCount = keys.maxWorkerCount;
+    var workerCount = keys.newWorkerCount;
+    $.Msg("Received worker update: (cur/max): "+workerCount + "/" + maxWorkerCount);
+    CheckHudFlipped();
+    $("#WorkerCount").text = workerCount + " / " + maxWorkerCount;
+}
+
+// Moves the clock panel to the correct side of the screen
+// if the HUD is flipped.
+function CheckHudFlipped() {
+    var workerPanel = $.FindChildInContext("#WorkerPanel");
+    
+    if (Game.IsHUDFlipped()) {
+	workerPanel.RemoveClass("Right");
+	workerPanel.AddClass("Flipped");
+    } else {
+	workerPanel.AddClass("Right");
+	workerPanel.RemoveClass("Flipped");
+    }
+}
+
+(function() {
+    $("#WorkerCount").text = "0 / 10";
+    GameEvents.Subscribe("new_worker_count", SetWorkerCount);
+    CheckHudFlipped();
+})();
